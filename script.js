a0b0335217d9b179192effdf4e53b16130423497
Aku Ocampo <akuocampo@gmail.com>
2023-10-31 22:45:45 -0300

    modificaciones de estilos

--- script.js
+++ script.js
@@ -8,12 +8,22 @@ function cambioEstado(){
     //Agarro el valor de la tarea seleccionada
     const valorTarea = tareaElegida.value;
     estadoElegido.textContent = valorTarea;
+
+    //Darle estilo según la opción elegida
+    if(valorTarea === 'Completada'){
+        estadoElegido.style.backgroundColor = 'green'
+        estadoElegido.style.color = 'white'
+    }else if (valorTarea === 'Pendiente'){
+        estadoElegido.style.backgroundColor = 'red'
+        estadoElegido.style.color = 'white'
+    }else{
+        estadoElegido.style.backgroundColor = 'yellow'
+        estadoElegido.style.color = 'black'
+    }
 }
 
 //Cambio el estado al oir un cambio en el select
 tareaElegida.addEventListener("change",cambioEstado);
 
 
-// tareaElegida.addEventListener("change",function(){
-//     estadoElegido.textContent= valorTarea;
-// })
+
